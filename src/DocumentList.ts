bb9721c99a2d92b6b74f992d6b4668df7ad86982
Patryk Szlachta <patryk.szla@interia.pl>
2020-06-15 22:18:31 +0200

    half of first task

--- src/DocumentList.ts
+++ src/DocumentList.ts
@@ -5,6 +5,7 @@ import { throws } from "assert";
 
 export class DocumentList {
     arrayOfDocuments: string[];
+
     getDocumentList():string[] {
          const newLocalStorage = new LocalStorage;
         //  newLocalStorage.getDocuments();
@@ -17,32 +18,62 @@ export class DocumentList {
     }
 
     render(): HTMLUListElement {
-        var link = document.createElement('a');
+        // var link = document.createElement('a');
         this.getDocumentList();
-        var newDiv = document.createElement('div');
-        newDiv.id = 'keys-wrapper';
-        document.getElementById('document-list-wrapper').append(newDiv);
+        // var div = document.createElement('div');
+        // div.id = 'keys-wrapper';
+        // document.getElementById('document-list-wrapper').append(div);
         
        var listOfKeys = document.createElement('ul');
         listOfKeys.id = 'list-of-keys';
-        newDiv.append(listOfKeys);
+        document.getElementById('document-list-wrapper').append(listOfKeys);
+        // div.append(listOfKeys);
 
         for(let i = 0; i < this.arrayOfDocuments.length; i++ ) {
-            var newDiv = document.createElement('div');
-            newDiv.className = 'links-wrapper';
+            var divForLink = document.createElement('div');
+            divForLink.className = 'links-wrapper';
+            divForLink.id = this.arrayOfDocuments[i];
+            document.getElementById('list-of-keys').append(divForLink);
             var newLi = document.createElement('li');
-            newLi.innerText = this.arrayOfDocuments[i];
-            listOfKeys.append(newDiv);
-            newDiv.append(newLi);
+            divForLink.append(newLi);
+            var link = document.createElement('a');
+            link.innerText = this.arrayOfDocuments[i];
+            link.href = "./index.html";
+            newLi.append(link);
+
+            var buttonDelete = document.createElement('button');
+            buttonDelete.innerText = 'Usun';
+            buttonDelete.className = 'button-delete'
+            newLi.append(buttonDelete);
+            buttonDelete.addEventListener('click', () => {
+                
+            //   this.documentId = this.arrayOfDocuments[i];
+            //   console.log(this.documentId);
+              this.removeDocument(this.arrayOfDocuments[i]);
+            })
+
+            // newLi.innerText = this.arrayOfDocuments[i];
+            // listOfKeys.append(divForLink);
+            // divForLink.append(newLi);
+
         }
         // this.arrayOfDocuments.forEach(element => {
         //     var newLi = document.createElement('li');
         //     newLi.innerText = element;
         //     listOfKeys.append(newLi);
-            
         // });
-        
        return listOfKeys
         
+    }
+
+    removeDocument(id:string) :void {
+        // id = this.documentId;
+        var newLocalStorage = new LocalStorage();
+        newLocalStorage.removeItem(id);
+        document.getElementById(id).remove();
+                // newLocalStorage.removeItem();
+        // document.removeChild()
+                console.log('button delete');
+                // divForLink.remove();
     }
 }
